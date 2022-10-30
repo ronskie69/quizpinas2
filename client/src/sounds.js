6621dd6473b0f436076ef256ad7958dc626b5429
ronskie69 <90790271+ronskie69@users.noreply.github.com>
2022-10-30 21:21:37 +0800

    Add files via upload

---
+++ client/src/sounds.js
@@ -0,0 +1,40 @@
+export const intro = {
+    //url: 'https://github.com/ronskie69/music/blob/main/Hysterical%20Laugh%20Sound%20Effect.mp3?raw=true',
+    url: 'https://github.com/ronskie69/music/blob/main/intro.mp3?raw=true',
+}
+
+export const mid = {
+    url: 'https://github.com/ronskie69/music/blob/main/mid.mp3?raw=true',
+}
+
+export const end = {
+    url: 'https://github.com/ronskie69/music/blob/main/end.mp3',
+}
+
+export const tama = {
+    url: 'https://github.com/ronskie69/music/blob/main/happy.mp3?raw=true',
+}
+
+export const mali = {
+    url: 'https://github.com/ronskie69/music/blob/main/sad.mp3?raw=true',
+}
+
+export const tapos_na = {
+    url: 'https://github.com/ronskie69/music/blob/main/Case%20Closed%20Theme%20Song.mp3?raw=true'
+}
+
+export const tatsulok = {
+    url: 'https://github.com/ronskie69/music/blob/main/Bamboo%20-%20Tatsulok.mp3?raw=true'
+}
+
+export const pinoy_ako = {
+    url: 'https://github.com/ronskie69/music/blob/main/Orange%20&%20Lemons%20-%20Pinoy%20Ako.mp3?raw=true'
+}
+
+export const upuan = {
+    url: 'https://github.com/ronskie69/music/blob/main/Gloc%209%20-%20Upuan.mp3?raw=true'
+}
+
+export const departure = {
+    url: 'https://github.com/ronskie69/music/blob/main/Naruto%20Shippuden%20OST%20-%20Departure%20To%20The%20Front%20Lines.mp3?raw=true'
+}
