970789971739f2c4727d78b1873f924d40f2dfd3
ronskie69 <90790271+ronskie69@users.noreply.github.com>
2022-10-31 16:36:46 +0800

    Add files via upload

--- client/src/sounds.js
+++ client/src/sounds.js
@@ -7,8 +7,12 @@ export const mid = {
     url: 'https://github.com/ronskie69/music/blob/main/mid.mp3?raw=true',
 }
 
+export const almost_end = {
+    url: 'https://github.com/ronskie69/music/blob/main/almost_end.mp3?raw=true',
+}
+
 export const end = {
-    url: 'https://github.com/ronskie69/music/blob/main/end.mp3',
+    url: 'https://github.com/ronskie69/music/blob/main/end.mp3?raw=true',
 }
 
 export const tama = {
@@ -37,4 +41,8 @@ export const upuan = {
 
 export const departure = {
     url: 'https://github.com/ronskie69/music/blob/main/Naruto%20Shippuden%20OST%20-%20Departure%20To%20The%20Front%20Lines.mp3?raw=true'
+}
+
+export const ph = {
+    url: 'https://github.com/ronskie69/music/blob/main/Magellan%20(Philippine%20History%20-%20Yoyoy%20Villame)%20Lyrics.mp3?raw=true'
 }
