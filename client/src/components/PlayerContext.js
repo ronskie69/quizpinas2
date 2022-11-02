443ed252b7ccb72aab6f9f159f850cc6485a1c9d
nogs <90790271+ronskie69@users.noreply.github.com>
2022-11-02 08:04:06 +0800

    Add files via upload

--- client/src/components/PlayerContext.js
+++ client/src/components/PlayerContext.js
@@ -3,14 +3,27 @@ import axios from 'axios'
 
 export const PContext = createContext(null)
 
+Array.min = function( array ){
+  return Math.min.apply( Math, array );
+};
+
 const PlayerContext = ({ children }) => {
 
     const [ players, setPlayers ] = useState([])
+    const [ lowestScore, setLowestScore ] = useState(0)
 
     useEffect(() => {
       axios.get('https://quizpinas2.herokuapp.com/quizpinas/')
         .then(response => {
+
+          let arr = []
           setPlayers(response.data)
+
+          response.data.find((p, i) => {
+            arr.push(p.player_score)
+          })
+
+         setLowestScore(Array.min(arr))
         })
         .catch(err => {
           console.log("Error: ", err)
@@ -18,7 +31,7 @@ const PlayerContext = ({ children }) => {
     },[0])
 
   return (
-    <PContext.Provider value={[ players, setPlayers ]}>
+    <PContext.Provider value={[ players, setPlayers, lowestScore ]}>
         {children}
     </PContext.Provider>
   )
